3b0ecb827e4f2f9bc94c326df9b9337983878a47
Ben Cartwright-Cox <Ben@Benjojo.co.uk>
2019-10-27 14:14:07 +0000

    Acivation works

--- driver/main.go
+++ driver/main.go
@@ -101,6 +101,78 @@ func main() {
 		log.Fatalf("Set a -stage1 or -stage2")
 	}
 
+	log.Printf("Activating in 2 seconds")
+	time.Sleep(time.Second * 2)
+
+	//  unable to grab default interface:
+	// vid=5555,pid=3382,bus=3,addr=63,config=1,if=0,alt=0
+	// does not have endpoint with address 0x80.
+	// Available endpoints: [0x82(2,IN)]
+
+	cfg, err := dev.Config(1)
+	if err != nil {
+		log.Fatalf("Kaboom, unable to grab Config(1): %s", err.Error())
+	}
+	fmt.Print("Config: ")
+
+	defaultinterface, err := cfg.Interface(0, 0)
+	if err != nil {
+		log.Fatalf("Kaboom, unable to grab Interface(0, 0): %s", err.Error())
+	}
+	fmt.Print("Interface: ")
+
+	// defaultInterface, done, err := dev.DefaultInterface()
+	// defer done()
+	// if err != nil {
+	// 	log.Fatalf("Kaboom, unable to grab default interface: %s", err.Error())
+	// }
+
+	inputTest, err := defaultinterface.InEndpoint(2)
+	if err != nil {
+		log.Fatalf("Kaboom, unable to grab default interface: %s", err.Error())
+	}
+
+	fmt.Print("Endpoint: ")
+
+	buf := make([]byte, 39)
+	_, err = dev.Control(0x40, 176, 0, 0, buf)
+	fmt.Print("Null 39 Control: ")
+
+	inBuf := make([]byte, 12)
+	_, err = dev.Control(0xc0, 177, 0, 0, inBuf)
+	fmt.Print("Grab In 12 Control: \n")
+
+	log.Printf("Here is a blob that I don't know what it does %x", inBuf)
+
+	// now to send what i think activates this
+
+	go func() {
+
+		for {
+			dev.Control(0x40, 176, 0, 0, []byte("\x04\x10\x00\x00\x10\x14\x10\xaa\xaa\xaa\x1f\x1f\x1f\x03\x03\x20\x02\x58\x00\x01\x02\x58\x00\x7c\x19\x01\x01\x80\x80\x00\x00\x00\x00\x00\x00\x03\x20\x02\x58"))
+			time.Sleep(time.Millisecond * 200)
+			dev.Control(0x40, 184, 0x0076, 0, []byte(""))
+		}
+
+	}()
+
+	f, _ := os.Create("./debug")
+
+	bytesRead := 0
+	for {
+		dataBuf := make([]byte, 61440*6)
+		n, err := inputTest.Read(dataBuf)
+		if err != nil {
+			log.Printf("failed to read aa %s", err)
+			continue
+		}
+
+		bytesRead += n
+
+		f.Write(dataBuf[:n])
+		fmt.Printf("\rBytes Read: %d   ", bytesRead)
+	}
+
 	time.Sleep(time.Hour)
 
 }
