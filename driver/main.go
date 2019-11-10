1908b4bb2cde8117a5a6f36d3488ce2826da67f3
Ben Cartwright-Cox <Ben@Benjojo.co.uk>
2019-11-10 18:37:12 +0000

    Reader that works!

--- driver/main.go
+++ driver/main.go
@@ -178,6 +178,8 @@ func main() {
 
 	//                                  "\x04\x10\x00\x00\x08\x14\x04\x9e\x9f\x9e\x1f\x1f\x1f\x03\x03\x20\x00\xc8\x00\x01\x00\xc8\x00\x98\x19\x01\x01\x80\x80\x05\x00\x00\x00\x00\x00\x03\x20\x00\xc8"
 	//	                                "\x04\x10\x00\x00\x08\x14\x04\x9b\x9e\x9e\x1f\x1f\x1f\x03\x03\x20\x02\x58\x00\x01\x02\x58\x00\x7c\x19\x01\x01\x80\x80\x05\x00\x00\x00\x00\x00\x03\x20\x02\x58"
+
+	// dev.Control(0x40, 176, 0, 0, []byte("\x04\x10\x00\x00\x08\x14\x1c\xba\xba\xbb\x1f\x1f\x1f\x03\x03\x20\x02\x58\x00\x01\x02\x58\x00\x98\x19\x01\x01\x80\x80\x05\x00\x00\x00\x00\x00\x03\x20\x02\x58"))
 	dev.Control(0x40, 176, 0, 0, []byte("\x04\x10\x00\x00\x08\x14\x1c\x90\x91\x91\x1f\x1f\x1f\x03\x03\x20\x02\x58\x00\x01\x02\x58\x00\x7c\x19\x01\x01\x80\x80\x05\x00\x00\x00\x00\x00\x03\x20\x02\x58"))
 	dev.Control(0x40, 184, 0x0076, 0, []byte(""))
 
@@ -204,30 +206,79 @@ func main() {
 	}
 
 	// Now a vauge attempt to reconstruct into a image?
-
-	PayloadB := PayloadB2[4:]
+	// 01 30 01 00
+	PayloadB := PayloadB2
 	img := image.NewNRGBA(image.Rect(0, 0, 800, 600))
 	log.Printf("len= %d vs %d", len(PayloadB), 800*600)
 	x, y := 0, 0
+	// debugf, _ := os.Create("debug-hex")
 
-	for n := 0; n < len(PayloadB); n = n + 3 {
-		if n+3 > len(PayloadB) {
-			break
-		}
+	// bread := 0
 
-		// for i := 0; i < 3; i++ {
+	rSk := ReadSkipper{
+		Data:  PayloadB,
+		Bread: 4,
+	}
+
+	bench := time.Now()
+
+	for {
+
+		RGB := rSk.ReadPixel()
+		R, G, B := color.YCbCrToRGB(RGB[0], RGB[1], RGB[2])
 		img.Set(x, y, color.NRGBA{
-			R: PayloadB[n],
-			G: PayloadB[n+1],
-			B: PayloadB[n+2],
+			R: B,
+			G: G,
+			B: R,
 			A: 255,
 		})
+
+		// debugf.WriteString(fmt.Sprintf("%d: %02x %02x %02x \n", y, RGB[0], RGB[1], RGB[2]))
+
 		x++
-		if x == 799 {
+		if x == 800 {
 			x = 0
 			y++
 		}
+		if y == 600 {
+			break
+		}
 	}
+
+	log.Printf("took %s to process that", time.Since(bench).String())
+	// for n := 3; n < len(PayloadB); n = n + 3 {
+	// 	if n+3 > len(PayloadB) {
+	// 		break
+	// 	}
+
+	// 	// for i := 0; i < 3; i++ {
+	// 	img.Set(x, y, color.NRGBA{
+	// 		R: PayloadB[n],
+	// 		G: PayloadB[n+1],
+	// 		B: PayloadB[n+2],
+	// 		A: 255,
+	// 	})
+
+	// 	if PayloadB[n] != 0x80 {
+	// 		debugf.WriteString(fmt.Sprintf("abormal read, Expected 80 got %02x at n=%d bread=%d\n", PayloadB[n], n, bread))
+	// 	}
+
+	// 	if PayloadB[n+1] != 0x80 {
+	// 		debugf.WriteString(fmt.Sprintf("abormal read, Expected 80 got %02x at n=%d bread=%d\n", PayloadB[n+1], n+1, bread+1))
+	// 	}
+
+	// 	if PayloadB[n+2] != 0xff {
+	// 		debugf.WriteString(fmt.Sprintf("abormal read, Expected ff got %02x at n=%d bread=%d\n", PayloadB[n+2], n+2, bread+2))
+	// 	}
+
+	// 	debugf.WriteString(fmt.Sprintf("%d: %02x (%d) %02x (%d) %02x (%d) [%d]\n", n, PayloadB[n], n, PayloadB[n+1], n+1, PayloadB[n+2], n+2, bread))
+	// 	x++
+	// 	if x == 799 {
+	// 		x = 0
+	// 		y++
+	// 	}
+	// 	bread += 3
+	// }
 	// for n, _ := range PayloadB {
 	// if n == len(PayloadB)-3 {
 	// 	break
@@ -252,6 +303,7 @@ func main() {
 	a, _ := os.Create("lol.png")
 	png.Encode(a, img)
 
+	log.Printf("Going to sleep, bye")
 	time.Sleep(time.Hour)
 
 }
@@ -263,4 +315,43 @@ type usbControlPacket struct {
 	WValue      uint16
 	WIndex      uint16
 	Data        []byte
+}
+
+type ReadSkipper struct {
+	Data  []byte
+	Bread int
+}
+
+var debug = false
+
+func (r *ReadSkipper) ReadPixel() [3]byte {
+	var output [3]byte
+
+	output[0] = r.readByte()
+	if output[0] != 0xff && debug {
+		fmt.Printf("Abormal read on R: %02x [%d]\n", output[0], r.Bread)
+	}
+	output[1] = r.readByte()
+	if output[1] != 0x80 && debug {
+		fmt.Printf("Abormal read on G: %02x [%d]\n", output[1], r.Bread)
+	}
+	output[2] = r.readByte()
+	if output[2] != 0x80 && debug {
+		fmt.Printf("Abormal read on B: %02x [%d]\n", output[2], r.Bread)
+	}
+
+	return output
+}
+
+func (r *ReadSkipper) readByte() byte {
+	r.Bread++
+	if r.Bread%1028 == 0 {
+		r.Bread += 4
+	}
+
+	if r.Bread > len(r.Data)-1 {
+		return 0x00
+	}
+
+	return r.Data[r.Bread]
 }
